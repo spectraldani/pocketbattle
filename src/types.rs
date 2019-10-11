d99f6054a998f4b6ea43ab797ce7540ee72961ad
Daniel Ramos <sentret@outlook.com>
2019-10-11 00:03:57 -0300

    Change TypeIndex to u8

--- src/types.rs
+++ src/types.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 
 #[derive(Debug,Deserialize)]
 struct SerializedTypes {
-    ammount: usize,
+    ammount: u8,
     names: Box<[String]>,
     chart: Vec<f32>
 }
@@ -13,7 +13,7 @@ pub struct Types {
     names: Box<[String]>,
     chart: Array2<f32>
 }
-pub type TypeIndex = usize;
+pub type TypeIndex = u8;
 
 #[derive(Debug)]
 pub enum MonsterType {
@@ -23,19 +23,20 @@ pub enum MonsterType {
 
 impl Types {
     pub fn get_pretty_name(&self,type_id: TypeIndex) -> &str {
-        &self.names[type_id]
+        &self.names[type_id as usize]
     }
     pub fn get_attack_effectiveness(&self,attacker: TypeIndex, defender: MonsterType) -> f32 {
         match defender {
-            MonsterType::Single(defender) => self.chart[[attacker, defender]],
+            MonsterType::Single(defender) => self.chart[[attacker as usize, defender as usize]],
             MonsterType::Double(main,sec) => {
-                self.chart[[attacker, main]] * self.chart[[attacker, sec]]
+                self.chart[[attacker as usize, main as usize]] *
+                self.chart[[attacker as usize, sec as usize]]
             }
         }
     }
     pub fn from_toml(raw_types: toml::Value) -> Types {
         let raw_types = raw_types.try_into::<SerializedTypes>().unwrap();
-        let dim = (raw_types.ammount, raw_types.ammount);
+        let dim = (raw_types.ammount as usize, raw_types.ammount as usize);
         Types {
             names: raw_types.names,
             chart: Array2::from_shape_vec(dim,raw_types.chart).expect(
